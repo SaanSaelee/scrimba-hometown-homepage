6c0876d744ac07fdf52bf1ab1f062628a778c084
pennyflipper.dev <76460018+SaanSaelee@users.noreply.github.com>
2022-09-26 10:31:13 -0700

    minor commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+// const menu = document.querySelector("#mobile-menu");
+// const menuLinks = document.querySelector(".nav-menu");
+
+// menu.addEventListener("click", function () {
+//     menu.classList.toggle("is-active");
+//     menuLinks.classList.toggle("acitve");
+// });
+
 // Modal items
 const modal = document.getElementById("modal-container");
 const openBtn = document.querySelector(".btn__sign-up");
